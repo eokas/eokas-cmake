ce20c30aadfa0ef97195ed6991a53c9968a6b72f
Qilin <qilin.cheng@foxmail.com>
2022-07-14 18:31:56 +0800

    fix

--- eokas_import_library.cmake
+++ eokas_import_library.cmake
@@ -5,8 +5,8 @@ macro(eokas_import_library LIBRARY_NAME LIBRARY_PATH, LIBRARY_TAG)
 
     FetchContent_Declare(
             ${LIBRARY_NAME}
-            GIT_REPOSITORY "${LIBRARY_PATH}"
-            GIT_TAG "${LIBRARY_TAG}"
+            GIT_REPOSITORY ${LIBRARY_PATH}
+            GIT_TAG ${LIBRARY_TAG}
     )
 
     FetchContent_MakeAvailable(${LIBRARY_NAME})
