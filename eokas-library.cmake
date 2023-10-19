ea61d664bb2632672d21a394932286c99e5c98e9
Qilin <qilin.cheng@foxmail.com>
2023-10-19 14:12:07 +0800

    cmake test

--- eokas-library.cmake
+++ eokas-library.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.14 )
+cmake_minimum_required(VERSION 3.22)
 
 macro(eokas_import_library LIBRARY_NAME LIBRARY_PATH, LIBRARY_TAG)
     include(FetchContent)
@@ -8,13 +8,17 @@ macro(eokas_import_library LIBRARY_NAME LIBRARY_PATH, LIBRARY_TAG)
             GIT_REPOSITORY ${LIBRARY_PATH}
             GIT_TAG ${LIBRARY_TAG}
     )
+endmacro()
+
+macro(eokas_using_library LIBRARY_NAME...)
+    include(FetchContent)
 
     FetchContent_MakeAvailable(${LIBRARY_NAME})
 
-    message("Import library: ${LIBRARY_NAME} => ${LIBRARY_PATH} : ${LIBRARY_TAG}")
-    message("${LIBRARY_NAME}_INCLUDE_DIR: ${${LIBRARY_NAME}_INCLUDE_DIR}")
+    message("Import library: ${LIBRARY_NAME} => ${LIBRARY_PATH}:${LIBRARY_TAG}")
+    message("${LIBRARY_NAME}_HEADER_DIR: ${${LIBRARY_NAME}_HEADER_DIR}")
     message("${LIBRARY_NAME}_SOURCE_DIR: ${${LIBRARY_NAME}_SOURCE_DIR}")
 
-    list(APPEND CMAKE_MODULE_PATH "${${LIBRARY_NAME}_INCLUDE_DIR}")
+    list(APPEND CMAKE_MODULE_PATH "${${LIBRARY_NAME}_HEADER_DIR}")
     list(APPEND CMAKE_MODULE_PATH "${${LIBRARY_NAME}_SOURCE_DIR}")
 endmacro()
--- index.cmake
+++
@@ -1,2 +0,0 @@
-
-include(${CMAKE_CURRENT_LIST_DIR}/eokas_import_library.cmake)
